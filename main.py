487807488758b7ee1f235fe8f3309e50500cfa01
PLkolek <mateusz.kolaczek@gmail.com>
2016-01-10 13:32:43 +0100

    Generating neighbourhood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from evaluate import evaluate
+import copy
 
 class Operation:
     def __init__(self, id, machine, time, job):
@@ -50,48 +51,73 @@ class Edge:
         return str(self.__dict__)
     def __repr__(self):
         return self.__str__()
+    def __eq__(self, other):
+        return self.target == other.target and self.reversable == other.reversable
 
 class Solution:
-    def __init__(self, problem):
-        self.problem = problem
+    @staticmethod
+    def initial(problem):
+        START = len(problem.operations)
+        END = len(problem.operations) + 1
 
-        self.START = len(problem.operations)
-        self.END = len(problem.operations) + 1
-
-        self.directed = [[] for x in range(0, len(problem.operations) + 2)]
-        self.directed[self.START] = [Edge(job[0], False) for job in problem.jobs]
+        directed = [[] for x in range(0, len(problem.operations) + 2)]
+        directed[START] = [Edge(job[0], False) for job in problem.jobs]
         for job in problem.jobs:
-            self.directed[job[-1]].append(Edge(self.END, False))
-        for (s, e) in self.__directedEdges(problem.jobs):
-            self.directed[s].append(Edge(e, False))
+            directed[job[-1]].append(Edge(END, False))
 
-        self.__initialSolution()
+        paired = [pair(job) for job in problem.jobs]
+        jobEdges = flatten(paired)
+        for (s, e) in jobEdges:
+            directed[s].append(Edge(e, False))
+
+        # Assumption: machine operations in problem are filled job by job
+        # Warning: it can create duplicate edges, is that a problem?
+        for ops in problem.machineOperations:
+            for (s, e) in pair(ops):
+                directed[s].append(Edge(e, True))
+        return Solution(problem, START, END, directed)
+
+    def __init__(self, problem, start, end, directed):
+        self.problem = problem
+        self.START = start
+        self.END = end
+        self.directed = directed
 
     def time(self, operationId):
         if operationId != solution.START and operationId != solution.END:
             return self.problem.operations[operationId].time
         return 0
 
-    def __initialSolution(self):
-        # Assumption: machine operations in problem are filled job by job
-        # Warning: it can create duplicate edges, is that a problem?
-        for ops in problem.machineOperations:
-            for (s, e) in pair(ops):
-                self.directed[s].append(Edge(e, True))
+    def neighbours(self, criticalPath):
+        neighbours = []
+        for (s, e) in pair(criticalPath):
+            if self.__hasReversableEdge(s, e):
+                neighbours.append(solution.__revertedEdge(s, e))
+        return neighbours
 
-    def __directedEdges(self, jobs):
-        paired = [pair(job) for job in jobs]
-        return flatten(paired)
+    def __hasReversableEdge(self, s, e):
+        return Edge(e, True) in self.directed[s]
+
+    def __revertedEdge(self, s, e):
+        edgesCopy = [copy.copy(edges) for edges in self.directed]
+        edgesCopy[s].remove(Edge(e, True))
+        edgesCopy[e].append(Edge(s, True))
+        return Solution(self.problem, self.START, self.END, edgesCopy)
 
     def __str__(self):
         return str(self.__dict__)
 
+    def __repr__(self):
+        return self.__str__()
 
 
 
 problem = Problem(2, [ [(0, 2), (1, 2), (0, 1)], [(1, 3), (0, 3), (0, 4)] ] )
-solution = Solution(problem)
+solution = Solution.initial(problem)
 print(problem)
 print(solution)
 
-print(evaluate(solution))
+(path, length) = evaluate(solution)
+print((path, length))
+
+print(solution.neighbours(path))
