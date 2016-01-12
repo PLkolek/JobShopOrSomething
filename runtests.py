798fac66850f33652c8f281e14a6253f3dfa2cc0
Radek <kedar03@gmail.com>
2016-01-12 17:19:31 +0100

    tests

---
+++ runtests.py
@@ -0,0 +1,33 @@
+import os
+from evaluate import evaluate
+from os.path import basename
+from main import solve
+
+def getDirContent(basePath):
+    return [os.path.join(basePath,f) for f in os.listdir(basePath)]
+
+def getSubdirectiories(basePath):
+    return filter(os.path.isdir,getDirContent(basePath))
+
+def getFiles(basePath):
+    return filter(os.path.isfile,getDirContent(basePath))
+
+def runTests(rirs):
+    baseResultPath = "results/"
+    for directory in dirs:
+        resultFilePath = baseResultPath + basename(directory)
+        resultFile = open(resultFilePath,"w")
+        testFilesNames = sorted(getFiles(directory))
+        for f in testFilesNames:
+            testFile = open(f,"r")
+            solution = solve(testFile)
+            resultFile.write(str(solution) + "\n")
+            print f + " " + str(solution)
+        resultFile.close()
+
+
+baseTestPath = "tests/"
+dirs = getSubdirectiories(baseTestPath)
+runTests(dirs)
+
+
